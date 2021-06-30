7ed2528e880dfc176992983001d5d30b930bc042
Thanh Le <lenguyenthanh@hotmail.com>
2021-06-30 10:24:09 +0200

    Add maven-publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ buildscript {
 
 plugins {
     kotlin("jvm") version "1.5.20"
+    `maven-publish`
 }
 
 apply(plugin = "kotlinx-atomicfu")
@@ -21,12 +22,21 @@ group = "com.lenguyenthanh.redux"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    mavenLocal()
     mavenCentral()
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
     // RxJava
     api("io.reactivex.rxjava2:rxjava:2.2.8")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("binary") {
+            from(components["java"])
+        }
+    }
 }
